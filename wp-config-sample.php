99ecd405e79bda26ac07e75b62a8a1460f01a7de
navinweb <anatoliykirzo@gmail.com>
2018-08-05 01:47:58 +0300

    change wp-config

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -5,25 +5,43 @@ define('WP_MAX_MEMORY_LIMIT', '3000M');
 
 define('WP_DEBUG', false);
 
+if ( WP_DEBUG ) {
+	define( 'DB_NAME', '' );
+	define( 'DB_USER', '' );
+	define( 'DB_PASSWORD', '' );
+
+	define( 'SMTP_HOST', '' );
+	define( 'SMTP_USERNAME', '' );
+	define( 'SMTP_PASSWORD', '' );
+	define( 'SMTP_SECURE', '' );
+} else {
+	define( 'DB_NAME', '' );
+	define( 'DB_USER', '' );
+	define( 'DB_PASSWORD', '' );
+
+	define( 'SMTP_HOST', 'smtp.gmail.com' );
+	define( 'SMTP_USERNAME', '' );
+	define( 'SMTP_PASSWORD', '' );
+	define( 'SMTP_SECURE', 'ssl' );
+}
+
+define( 'WP_LANG', 'en_EN' );
+
+define( 'DB_HOST', 'localhost' );
+
 define('WP_ADMIN_BAR', true);
 define('DISALLOW_FILE_MODS', false);
-define('WP_VERSION', '1.0.0');
+
+define( 'WP_DEBUG_LOG', true );
+define( 'WP_DEBUG_DISPLAY', false );
 
 define('WP_HTTP_BLOCK_EXTERNAL', false);
 
 define('WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST']);
 define('WP_HOME', 'http://' . $_SERVER['HTTP_HOST']);
+define('WP_VERSION', '1.0.0');
 
-define('DB_NAME', 'database_name_here');
-
-define('DB_USER', 'username_here');
-
-define('DB_PASSWORD', 'password_here');
-
-define('DB_HOST', 'localhost');
-
-define('DB_CHARSET', 'utf8');
-
+define('DB_CHARSET', '');
 define('DB_COLLATE', '');
 
 define('AUTH_KEY',         'put your unique phrase here');
