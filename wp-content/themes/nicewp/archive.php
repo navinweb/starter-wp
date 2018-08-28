16ca71d1beff1b8862c40ddaded58cb25ba9f2c6
navinweb <anatoliykirzo@gmail.com>
2018-08-28 21:33:35 +0300

    remove comments

--- wp-content/themes/nicewp/archive.php
+++ wp-content/themes/nicewp/archive.php
@@ -12,18 +12,11 @@ get_header();
 				the_archive_title( '<h1 class="page-title">', '</h1>' );
 				the_archive_description( '<div class="archive-description">', '</div>' );
 				?>
-			</header><!-- .page-header -->
+			</header>
 
 			<?php
-			/* Start the Loop */
 			while ( have_posts() ) :
 				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
 				get_template_part( 'template-parts/content', get_post_type() );
 
 			endwhile;
@@ -37,8 +30,8 @@ get_header();
 		endif;
 		?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		</main>
+	</div>
 
 <?php
 get_sidebar();
