f7feb08106dd2d10bc36d70e4b57723fb74d9a51
navinweb <anatoliykirzo@gmail.com>
2018-08-30 01:04:54 +0300

    remove comments

--- wp-content/themes/nicewp/footer.php
+++ wp-content/themes/nicewp/footer.php
@@ -4,18 +4,16 @@
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'nicewp' ) ); ?>">
 				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
 				printf( esc_html__( 'Proudly powered by %s', 'nicewp' ), 'WordPress' );
 				?>
 			</a>
 			<span class="sep"> | </span>
 				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
 				printf( esc_html__( 'Theme: %1$s by %2$s.', 'nicewp' ), 'nicewp', '<a href="http://navinweb.net/">Anatolii Kirzo</a>' );
 				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+		</div>
+	</footer>
+</div>
 
 <?php wp_footer(); ?>
 
