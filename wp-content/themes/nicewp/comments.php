69a635e78e9f73f452371423da6590128a62098d
navinweb <anatoliykirzo@gmail.com>
2018-09-04 20:22:04 +0300

    remove comments

--- wp-content/themes/nicewp/comments.php
+++ wp-content/themes/nicewp/comments.php
@@ -1,9 +1,5 @@
 <?php
-/*
- * If the current post is protected by a password and
- * the visitor has not yet entered the password we will
- * return early without loading the comments.
- */
+
 if ( post_password_required() ) {
 	return;
 }
@@ -12,7 +8,6 @@ if ( post_password_required() ) {
 <div id="comments" class="comments-area">
 
 	<?php
-	// You can start editing here -- including this comment!
 	if ( have_comments() ) :
 		?>
 		<h2 class="comments-title">
@@ -20,20 +15,18 @@ if ( post_password_required() ) {
 			$nicewp_comment_count = get_comments_number();
 			if ( '1' === $nicewp_comment_count ) {
 				printf(
-					/* translators: 1: title. */
 					esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'nicewp' ),
 					'<span>' . get_the_title() . '</span>'
 				);
 			} else {
-				printf( // WPCS: XSS OK.
-					/* translators: 1: comment count number, 2: title. */
+				printf(
 					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $nicewp_comment_count, 'comments title', 'nicewp' ) ),
 					number_format_i18n( $nicewp_comment_count ),
 					'<span>' . get_the_title() . '</span>'
 				);
 			}
 			?>
-		</h2><!-- .comments-title -->
+		</h2>
 
 		<?php the_comments_navigation(); ?>
 
@@ -44,21 +37,20 @@ if ( post_password_required() ) {
 				'short_ping' => true,
 			) );
 			?>
-		</ol><!-- .comment-list -->
+		</ol>
 
 		<?php
 		the_comments_navigation();
 
-		// If comments are closed and there are comments, let's leave a little note, shall we?
 		if ( ! comments_open() ) :
 			?>
 			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'nicewp' ); ?></p>
 			<?php
 		endif;
 
-	endif; // Check for have_comments().
+	endif;
 
 	comment_form();
 	?>
 
-</div><!-- #comments -->
+</div>
